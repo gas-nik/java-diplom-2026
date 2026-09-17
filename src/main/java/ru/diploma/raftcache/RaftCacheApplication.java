31faec9ca1026cd517b573d3e80a6c34e8e8f5da
GAS <nikitashaitor@yandex.ru>
2026-09-17 20:19:48 +0300

    done diploma

---
+++ src/main/java/ru/diploma/raftcache/RaftCacheApplication.java
@@ -0,0 +1,11 @@
+package ru.diploma.raftcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "ru.diploma.raftcache")
+public class RaftCacheApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RaftCacheApplication.class, args);
+	}
+}
